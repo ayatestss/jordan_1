53cbf72304b699774e8208c65cf7bfa81fe5e7a3
AlanisYates <yatesalanis@gmail.com>
2023-06-18 21:29:02 -0400

    sync complete 6-18-2023

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import ErrorPage from "./pages/ErrorPage";
 import ConfirmationPage from "./pages/ComingSoon/ConfirmationPage";
 import EmailSignup from "./pages/ComingSoon/EmailSignup";
+import Dashboard from "./dashboard/Dashboard";
+import Services from "./pages/services";
+import Invoices from "./pages/invoices";
+import MemberSettings from "./pages/membersettings";
+import MemberChat from "./pages/MemberChat/MemberChat";
+import FAQ from "./pages/faq";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -17,6 +23,12 @@ function App() {
             <Route path="/" element={<EmailSignup />} />
             <Route path="/confirmationPage" element={<ConfirmationPage />} />
             <Route path="*" element={<ErrorPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/membersettings" element={<MemberSettings />} />
+            <Route path="/MemberChat" element={<MemberChat />} />
           </Routes>
         </div>
       </ThemeProvider>
